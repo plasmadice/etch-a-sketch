93431ab2a8e31293b70e4227f155777bd2cbfb1f
Herman White <phoenixquint@gmail.com>
2017-12-11 19:27:45 -0800

    Initial commit, working model

---
+++ README.md
@@ -0,0 +1,10 @@
+This project is still a work-in-progress with additional features to come.
+
+Works in progress include two additional versions of the page-
+-Version where mouseover generates random colors
+-Version where mouseover shades in black
+
+This project gave me a deeper understanding of eventlisteners. That was especially challenging. A lot was learned here about how to manipulate the DOM with javascript. Splitting CSS/Javascript/HTMl also intruduced the need to debug. I use a lot of unusual methods to do things that I'm sure will be extremely ugly to me later.
+
+Revisions:
+12/11/17: Initial commit, working model
---
+++ scripts/javascript.js
@@ -0,0 +1,59 @@
+let container;
+let grid;
+let grids = 0; // # of columns
+let columns = ''; // holds 'auto' for style of grids
+let targetGrid;
+let element;
+container = document.querySelector('.grid-container');
+
+function createGrid(a) { // creates each grid
+    wipeGrid();
+    a = prompt('What size will the grid be? (enter a value)','');
+    grids = a;
+    drawGrid(grids);
+    addAuto(grids);
+    finishGrid();
+}
+
+window.onmouseover=function(e) {
+    target = e.target.className;
+    targetGrid = document.querySelector(`.${target}`)
+    
+};
+
+document.addEventListener("mouseover", function(a){
+    if (target !== 'grid-container' && target !== '.' && target !== '') {
+        targetGrid.style.backgroundColor = 'black';
+    }
+});
+
+function drawGrid() {
+    for (i = 0; i < grids*grids; i++) {
+        grid = document.createElement('div');
+        grid.classList.add(`grid${i+1}`);
+        container.appendChild(grid);
+    }
+}
+
+function finishGrid() {
+    container.style.backgroundColor = 'black';
+    container.style.gridTemplateColumns =`${columns}`;
+}
+
+function addAuto() {
+    for (i = 0; i < grids; i++) { // adds 'auto' to columns
+    columns += ' auto';
+    }
+}
+
+function wipeGrid() {
+    element = document.getElementById("grid-container");
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+      }
+    container.style.backgroundColor = 'white';
+    columns = '';
+}
+
+
+
